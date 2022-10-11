46971e21bd3080d3ed9918a5220481b58d06ae77
Ripon Sk <riponsk477@gmail.com>
2022-10-11 20:29:56 +0530

    Initial Commit

---
+++ multiple collectionview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  multiple collectionview
+//
+//  Created by Ripon sk on 11/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
